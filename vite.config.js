82f784ae18b5b781a53b8fcc2bf48395ee5d2b27
003falcon <333mohammedmohtasim@gmail.com>
2026-03-24 23:06:15 +0530

    ci: GitHub Actions deploy + custom domain setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/bdacc-website/',
   plugins: [react()],
+  base: '/',   
 })
+
